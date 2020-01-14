5fe21c28e8c9b8317cde49deb6fd1dce4752e4a0
agencia <matheus.ribeiro@novaandradina.org>
2020-01-14 16:55:40 -0400

    added login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # Root
   root'welcome#home'
+  
+  # About page
   get '/about', to: 'welcome#about'
+
+  # User login, session create and destroy
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
 
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
