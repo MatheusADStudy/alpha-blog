4fb3415d575344159ca8e00a24300230f0b7ffe1
agencia <matheus.ribeiro@novaandradina.org>
2020-01-15 16:11:42 -0400

    links and user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only:[:edit, :update, :show]
+
+  def set_user
+    @user = User.find(params[:id])    
+  end
+
   def new
     @user = User.new
   end
@@ -19,12 +26,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
+    # @user = User.find(params[:id])
     if @user.update(user_params)
       debugger
       flash[:success] = "Your account was updated succesfully"
@@ -35,7 +41,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
