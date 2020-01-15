27700cf7a88d1086d27ad6214b4ceece46773741
agencia <matheus.ribeiro@novaandradina.org>
2020-01-15 16:41:59 -0400

    completed authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :show, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @article = Article.new
@@ -47,6 +49,14 @@ class ArticlesController < ApplicationController
     flash[:danger] = "Article was successfully deleted  "
     redirect_to articles_path
   end
+
+  def require_same_user
+    if current_user != @article.user
+    flash[:danger] = "You can only edit or delete your own articles"
+    redirect_to root_path
+    end
+  end
+
 
   private 
     def set_article
