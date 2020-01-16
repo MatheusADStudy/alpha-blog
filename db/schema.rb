731e05ff89139fff7dd4084f97d65fad50951242
agencia <matheus.ribeiro@novaandradina.org>
2020-01-16 10:52:30 -0400

    add admin user and funcionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200116123313) do
+ActiveRecord::Schema.define(version: 20200116141649) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20200116123313) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "email"
+    t.boolean  "admin",           default: false
   end
 
 end
